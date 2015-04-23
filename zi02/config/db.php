a7f1e238ed42b841c3407d4d8151fe6e6b0093f4
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-04-23 13:33:04 +0200

    Usunięto projekt zi2, zainicjowano Yii2 szkielet advanced.

--- zi02/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=apl-baz-pr2',
-    'username' => 'root',
-    'password' => 'usbw',
-    'charset' => 'utf8',
-];
