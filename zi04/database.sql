cd70c97489ed12f4cf374d3c48a52a1ccef1b9b2
Maciej Klemarczyk <m.klemarczyk@live.com>
2015-05-28 09:53:28 +0200

    User avatar może być teraz null, dla użytkowników, którzy nie mają zdjęcia.

--- zi04/database.sql
+++ zi04/database.sql
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `birthdate` date NOT NULL,
   `is_player` bit(1) NOT NULL,
-  `avatar` blob NOT NULL,
+  `avatar` blob NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
